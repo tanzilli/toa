1607cf4218141fc2f26e72d1b70d7ae3e907dbdd
Sergio Tanzilli <tanzilli@acmesystems.it>
2019-03-18 12:41:13 +0100

    twidgets

--- www/js/twidgets.js
+++ www/js/twidgets.js
@@ -1,4 +1,88 @@
-// tanzoClock
+// **************
+// tanzoSwitch
+// **************
+
+class tanzoSwitch {
+	constructor(caption,address,domElementId) {
+		this.domElementId = domElementId;
+		this.address = address;
+		this.state = 0;
+
+		$("#" + this.domElementId).click(this.click_handler.bind(this));
+		
+		$("#" + this.domElementId).html(`
+			<div id=` + address + ` style="
+				border: 2px solid black; 
+				background-color:rgba(10, 10, 100, 0.5); 
+				//border-color: red; 
+				box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+				width: 260px; 
+				height: 55px;
+				line-height: 55px;
+				padding-top: 4px;
+				padding-bottom: 4px;
+				margin-bottom: 5px;
+				font-family: arial;
+				font-size: 28px; 
+				color: white; 
+				padding-left: 20px;
+			">
+		    	` + caption + `
+			</div>
+		`);
+		
+		this.mqtt_client = new Paho.MQTT.Client(mqtt_broker, Number(mqtt_port), "/ws",randomString(20));
+		this.mqtt_client.onMessageArrived = this.onMessageArrived.bind(this);
+
+		this.mqtt_client.connect({
+			onSuccess:this.onConnect.bind(this),reconnect:true,keepAliveInterval: 3, timeout:3
+		});
+	}
+
+
+	onConnect() {
+		this.mqtt_client.subscribe("toa/lights/"+ this.address + "/current_value");
+		console.log("Connect");
+	}	
+
+	onMessageArrived(mqtt_message) {
+		if (mqtt_message.payloadString=="0") {
+			this.state_off();
+		} else {
+			this.state_on();
+		}
+	}
+	
+	click_handler() {
+		var message;
+		
+		if (this.state===0) {
+			message = new Paho.MQTT.Message("255");
+		} else {
+			message = new Paho.MQTT.Message("0");
+		}	
+		message.destinationName = "toa/lights/"+ this.address + "/set_value";
+		this.mqtt_client.send(message);
+	}
+
+	state_off() {
+		$("#" +  this.address).css("background-color","rgba(80,80,80, 0.5)");
+		$("#" +  this.address).css("color","white");
+		$("#" +  this.address).css("border-color","darkgrey");
+		this.state=0;
+	}
+
+	state_on() {
+		$("#" +  this.address).css("background-color","rgba(255, 255, 0, 0.7)");
+		$("#" +  this.address).css("color","black");
+		$("#" +  this.address).css("border-color","red");
+		this.state=1;
+	}
+}
+
+// **************
+//  tanzoClock
+// **************
 
 class tanzoClock {
 	constructor(domElementId,topline,bottomline) {
@@ -10,6 +94,90 @@ class tanzoClock {
 		});
 	}
 }
+
+// **************
+//  tanzoDate
+// **************
+
+var days = ["Domenica","Lunedi","Martedi","Mercoledi","Giovedi","Venerdi","Sabato"];
+var months = ["Gennaio","Febbraio","Marzo","Aprile","Maggio","Giugno","Luglio","Agosto","Settembre","Ottobre","Novembre","Dicembre"];
+
+class tanzoDate {
+	constructor(domElementId) {
+		this.updateDate(domElementId);
+		setInterval(this.updateDate,1000,domElementId);
+	}
+
+	updateDate(domElementId) {
+		var d = new Date();
+		$("#" + domElementId).html(days[d.getDay()] + " " +  d.getDate() + " " + months[d.getMonth()] + " " + d.getFullYear())
+		//$("#" + domElementId).html(d.getSeconds());
+	}
+}	
+
+// **************
+//  tanzoMessage
+// **************
+
+// Note: Uso della funzione bind() 
+// https://stackoverflow.com/questions/42233090/access-class-properties-outside-of-mqtt-callback-scope
+
+class tanzoMessage {
+	constructor(target) {
+		this.target = target;
+		
+		$("#" + this.target).attr("style",`
+		    position: fixed;
+		    color: white;
+		    width: 100%;
+		    height: 24px;
+		    padding: 5px;
+		    padding-left: 10px;
+		    padding-right: 10px;
+		    left: 0px;
+		    bottom: 0px;
+		    background-color: rgba(0, 127, 127, 0.5);
+		    font-family: arial;
+		    font-size: 20px;
+		`);
+		
+		$("#" + this.target).html(`
+			<span id="` + this.target + "_span" + `" style="position: relative;"></span>
+		`);
+
+		//*****************************************************************************
+		// Connessione al broker MQTT
+		// https://www.eclipse.org/paho/clients/js/
+		//*****************************************************************************
+	
+		this.mqtt_client = new Paho.MQTT.Client(mqtt_broker, Number(mqtt_port), "/ws",randomString(20));
+		this.mqtt_client.onMessageArrived = this.onMessageArrived.bind(this);
+		this.mqtt_client.connect({
+			onSuccess:this.onConnect.bind(this)
+		});
+	}
+	
+	message(text) {
+		this.text=text;
+		$("#" + this.target + "_span").animate({opacity: 0.0,bottom: '+=10px'}, "fast");
+		setTimeout(this.tickerIn, 500,this.target,this.text)
+		
+		//$("#" + this.target + "_span").html(text);
+	}
+
+	tickerIn(target,text) {
+		$("#" + target + "_span").html(text);
+		$("#" + target + "_span").animate({opacity: 1.0, bottom: '-=10px'}, "slow");
+	}
+
+	onConnect() {
+		this.mqtt_client.subscribe(mqtt_topic);
+	}	
+
+	onMessageArrived(mqtt_message) {
+		this.message(mqtt_message.destinationName + " " + mqtt_message.payloadString);	
+	}
+}	
 
 // thooClock, a jQuery Clock with alarm function
 // by Thomas Haaf aka thooyork, http://www.smart-sign.com
